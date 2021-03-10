b54ea3b2f09d56154980278aebffbab0f2f04887
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-03-10 11:44:31 +0200

    init: solve human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let upToTwenty = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let nToStr = number.toString();
+    if (number === 0) {
+        return "zero";
+    }
+    if (number < 20) {
+        return upToTwenty[number];
+    }
+    if (nToStr.length === 2) {
+        return (tens[nToStr[0]] + ' ' + upToTwenty[nToStr[1]]).trim();
+    }
+    if (nToStr.length === 3) {
+        if (nToStr[1] === "0" && nToStr[2] === "0") {
+            return upToTwenty[nToStr[0]] + ' hundred';
+        } else
+        return upToTwenty[nToStr[0]] +
+                ' hundred ' +
+                toReadable(+(nToStr[1] + nToStr[2]));
+    }
+};
